b4f4abba5c964da6d6c37f9e93a0429d05072b5d
Mason <changmason@gmail.com>
2018-02-04 21:52:53 +0800

    use cookbook file for remote_syslog2 package rather than remote file

--- papertrail/recipes/default.rb
+++ papertrail/recipes/default.rb
@@ -1,16 +1,13 @@
 case node['platform']
-when 'redhat', 'centos', 'amazon'
-  full_pkg_name = "remote_syslog2-#{node['papertrail']['version']}-1.x86_64.rpm"
-  pkg_name = 'remote_syslog2'
 when 'debian', 'ubuntu'
-  full_pkg_name = "remote-syslog2_#{node['papertrail']['version']}_amd64.deb"
+  full_pkg_name = "remote-syslog2_0.19_amd64.deb"
   pkg_name = 'remote-syslog2'
 else
   raise "Unsupported platform: #{node['platform']}"
 end
 
-remote_file "#{Chef::Config[:file_cache_path]}/#{full_pkg_name}" do
-  source "https://github.com/papertrail/remote_syslog2/releases/download/v#{node['papertrail']['version']}/#{full_pkg_name}"
+cookbook_file "#{Chef::Config[:file_cache_path]}/#{full_pkg_name}" do
+  source "#{full_pkg_name}"
   owner 'root'
   group 'root'
   mode '0644'
@@ -18,11 +15,6 @@ remote_file "#{Chef::Config[:file_cache_path]}/#{full_pkg_name}" do
 end
 
 case node['platform']
-when 'redhat', 'centos', 'amazon'
-  package 'remote_syslog2' do
-    action :install
-    source "#{Chef::Config[:file_cache_path]}/#{full_pkg_name}"
-  end
 when 'debian', 'ubuntu'
   dpkg_package 'remote-syslog2' do
     action :install
