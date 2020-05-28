21b2aa26106d9deac8877604a0eb3cc4ce15e87f
shes50103 <shes50103@gmail.com>
2020-05-28 17:49:53 +0800

    go213

--- opsworks_commons/libraries/monkey_patch_rubygems_provider.rb
+++ opsworks_commons/libraries/monkey_patch_rubygems_provider.rb
@@ -91,21 +91,39 @@ class Chef
 
         puts 'gogogogogo'
 
-        def install_via_gem_command(name, version)
-          puts 'gogogogogo123'
+        # def install_via_gem_command(name, version)
+        #   puts 'gogogogogo123'
 
-          src = []
-          if new_resource.source.is_a?(String) && new_resource.source =~ /\.gem$/i
-            name = new_resource.source
+        #   src = []
+        #   if new_resource.source.is_a?(String) && new_resource.source =~ /\.gem$/i
+        #     name = new_resource.source
+        #   else
+        #     src << "--clear-sources" if new_resource.clear_sources
+        #     src += gem_sources.map { |s| "--source=#{s}" }
+        #   end
+        #   src_str = src.empty? ? "" : " #{src.join(" ")}"
+        #   if !version.nil? && !version.empty?
+        #     shell_out!("#{gem_binary_path} install #{name} -q -no-rdoc -no-ri -v \"#{version}\"#{src_str}#{opts}", env: nil)
+        #   else
+        #     shell_out!("#{gem_binary_path} install \"#{name}\" -q -no-rdoc -no-ri #{src_str}#{opts}", env: nil)
+        #   end
+        # end
+
+        def install_via_gem_command(name, version)
+          puts 'gogogogogo123 new'
+
+          if @new_resource.source =~ /\.gem$/i
+            name = @new_resource.source
+          elsif @new_resource.clear_sources
+            src = " --clear-sources"
+            src << (@new_resource.source && " --source=#{@new_resource.source}" || "")
           else
-            src << "--clear-sources" if new_resource.clear_sources
-            src += gem_sources.map { |s| "--source=#{s}" }
+            src = @new_resource.source && " --source=#{@new_resource.source} --source=#{Chef::Config[:rubygems_url]}"
           end
-          src_str = src.empty? ? "" : " #{src.join(" ")}"
-          if !version.nil? && !version.empty?
-            shell_out!("#{gem_binary_path} install #{name} -q -no-rdoc -no-ri -v \"#{version}\"#{src_str}#{opts}", env: nil)
+          if !version.nil? && version.length > 0
+            shell_out_with_timeout!("#{gem_binary_path} install #{name} -q -no-rdoc -no-ri -v \"#{version}\"#{src}#{opts}", :env => nil)
           else
-            shell_out!("#{gem_binary_path} install \"#{name}\" -q -no-rdoc -no-ri #{src_str}#{opts}", env: nil)
+            shell_out_with_timeout!("#{gem_binary_path} install \"#{name}\" -q -no-rdoc -no-ri #{src}#{opts}", :env => nil)
           end
         end
 
