63bc804a879214b507a9f68fffc0b190dcb47d44
shes50103 <shes50103@gmail.com>
2020-05-28 16:35:15 +0800

    go

--- opsworks_commons/libraries/monkey_patch_rubygems_provider.rb
+++ opsworks_commons/libraries/monkey_patch_rubygems_provider.rb
@@ -81,6 +81,24 @@ class Chef
             refresh_ruby_load_path
             return gem_specs
           end
+
+          puts 'gogogogogo'
+
+          def install_via_gem_command(name, version)
+            src = []
+            if new_resource.source.is_a?(String) && new_resource.source =~ /\.gem$/i
+              name = new_resource.source
+            else
+              src << "--clear-sources" if new_resource.clear_sources
+              src += gem_sources.map { |s| "--source=#{s}" }
+            end
+            src_str = src.empty? ? "" : " #{src.join(" ")}"
+            if !version.nil? && !version.empty?
+              shell_out!("#{gem_binary_path} install #{name} -q -no-rdoc -no-ri -v \"#{version}\"#{src_str}#{opts}", env: nil)
+            else
+              shell_out!("#{gem_binary_path} install \"#{name}\" -q -no-rdoc -no-ri #{src_str}#{opts}", env: nil)
+            end
+          end
         end
 
         class AlternateGemEnvironment < GemEnvironment
