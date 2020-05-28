003f900c000a3d1cfcb95976326df50893480f79
shes50103 <shes50103@gmail.com>
2020-05-28 17:30:28 +0800

    go213

--- opsworks_commons/libraries/monkey_patch_rubygems_provider.rb
+++ opsworks_commons/libraries/monkey_patch_rubygems_provider.rb
@@ -81,31 +81,31 @@ class Chef
             refresh_ruby_load_path
             return gem_specs
           end
-
-          puts 'gogogogogo'
-
-          def install_via_gem_command(name, version)
-            puts 'gogogogogo123'
-
-            src = []
-            if new_resource.source.is_a?(String) && new_resource.source =~ /\.gem$/i
-              name = new_resource.source
-            else
-              src << "--clear-sources" if new_resource.clear_sources
-              src += gem_sources.map { |s| "--source=#{s}" }
-            end
-            src_str = src.empty? ? "" : " #{src.join(" ")}"
-            if !version.nil? && !version.empty?
-              shell_out!("#{gem_binary_path} install #{name} -q -no-rdoc -no-ri -v \"#{version}\"#{src_str}#{opts}", env: nil)
-            else
-              shell_out!("#{gem_binary_path} install \"#{name}\" -q -no-rdoc -no-ri #{src_str}#{opts}", env: nil)
-            end
-          end
         end
 
         class AlternateGemEnvironment < GemEnvironment
           def gem_specification
             GemEnvironment.gem_specification_from_filesystem(gem_paths)
+          end
+        end
+
+        puts 'gogogogogo'
+
+        def install_via_gem_command(name, version)
+          puts 'gogogogogo123'
+
+          src = []
+          if new_resource.source.is_a?(String) && new_resource.source =~ /\.gem$/i
+            name = new_resource.source
+          else
+            src << "--clear-sources" if new_resource.clear_sources
+            src += gem_sources.map { |s| "--source=#{s}" }
+          end
+          src_str = src.empty? ? "" : " #{src.join(" ")}"
+          if !version.nil? && !version.empty?
+            shell_out!("#{gem_binary_path} install #{name} -q -no-rdoc -no-ri -v \"#{version}\"#{src_str}#{opts}", env: nil)
+          else
+            shell_out!("#{gem_binary_path} install \"#{name}\" -q -no-rdoc -no-ri #{src_str}#{opts}", env: nil)
           end
         end
 
