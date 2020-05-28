15b2549227480fc5b7d7e13a68753d5a3914b274
shes50103 <shes50103@gmail.com>
2020-05-28 10:43:04 +0800

    tmp

---
+++ opsworks_commons/libraries/monkey_patch_chefgem_resource.rb
@@ -0,0 +1,15 @@
+class Chef
+  class Resource
+    class ChefGem < Chef::Resource::Package::GemPackage
+      ##
+      # Options for the gem install, either a Hash or a String. When a hash is
+      # given, the options are passed to Gem::DependencyInstaller.new, and the
+      # gem will be installed via the gems API. When a String is given, the gem
+      # will be installed by shelling out to the gem command. Using a Hash of
+      # options with an explicit gem_binary will result in undefined behavior.
+      def options(opts={:opts => "-no-ri -no-rdoc"})
+        set_or_return(:options,opts,:kind_of => [String,Hash])
+      end
+    end
+  end
+end
